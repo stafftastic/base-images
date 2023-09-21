f8a0ffd56e9e8fb7aa2f6761894d5a0be0c0da40
Chris <chris@echoz.io>
2023-09-21 14:59:03 +0200

    feat: add nodejs base image

---
+++ nodejs/default.nix
@@ -0,0 +1,42 @@
+{ nodejs
+, busybox
+, curl
+, buildEnv
+, dockerTools
+, imageName ? "webapp-base-image"
+, imageTag ? "local"
+, extraEnv ? []
+, extraPkgs ? []
+}: let
+  bin = buildEnv {
+    name = "bin";
+    paths = [
+      busybox
+      curl
+      nodejs
+    ];
+    pathsToLink = [ "/bin" ];
+  };
+in dockerTools.buildImage {
+  name = imageName;
+  tag = imageTag;
+  copyToRoot = buildEnv {
+    name = "webapp-base";
+    paths = with dockerTools; [
+      bin
+      usrBinEnv
+      caCertificates
+    ] ++ extraPkgs;
+  };
+  runAsRoot = ''
+    #!/bin/sh
+    mkdir -pm1777 /tmp
+    mkdir -p /app
+    corepack enable
+  '';
+  config = {
+    Cmd = [ "/bin/sh" ];
+    WorkingDir = "/app";
+    Env = extraEnv;
+  };
+}
