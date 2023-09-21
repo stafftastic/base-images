275fc1e33e0053d92f71d246a90573f1ee41d76d
Chris <chris@echoz.io>
2023-09-21 14:58:45 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems }:
+  let
+    lib = nixpkgs.lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system: nixpkgs.legacyPackages.${system});
+  in {
+    packages = eachSystem (system: let
+      pkgs = pkgsFor.${system};
+      laravel = pkgs.callPackage ./laravel;
+      nodejs = pkgs.callPackage ./nodejs;
+    in {
+      laravel = laravel {};
+      laravelPhp74 = laravel { php = pkgs.php74; };
+      laravelPhp80 = laravel { php = pkgs.php80; };
+      laravelPhp81 = laravel { php = pkgs.php81; };
+      laravelPhp82 = laravel { php = pkgs.php82; };
+      laravelPhp83 = laravel { php = pkgs.php83; };
+      nodejs = nodejs {};
+      nodejs16 = nodejs { nodejs = pkgs.nodejs-16_x; };
+      nodejs18 = nodejs { nodejs = pkgs.nodejs-18_x; };
+      nodejs20 = nodejs { nodejs = pkgs.nodejs-20_x; };
+    });
+  };
+}
